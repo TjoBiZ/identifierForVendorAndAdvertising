e04e0fcf62a0604d6bf1c8182e7cfc28075d13ff
Roman <j@tjo.biz>
2021-05-12 06:10:55 +0800

    Get unique identifier For Advertising and for server identifierForVendor SWIFT

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import AdSupport
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var indentifier: UILabel!
+    
+    @IBOutlet weak var indentifierAdvertising: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        func identifierForAdvertising() -> String? {
+            // Check whether advertising tracking is enabled
+            guard ASIdentifierManager.shared().isAdvertisingTrackingEnabled else {
+                return nil
+            }
+            
+            // Get and return IDFA
+            return ASIdentifierManager.shared().advertisingIdentifier.uuidString
+        }
+        
+        let idAdvertising:String? = identifierForAdvertising()
+        
+        if let idAdver = idAdvertising {
+            indentifierAdvertising.text = idAdver
+        } else {
+            indentifierAdvertising.text = "It doesn't work"
+        }
+        
+        //let testUUID = NSUUID().uuidString //This method was use before with Objective-C
+        //let testUUID = UIDevice.currentDevice().identifierForVendor.UUIDString // Maybe this method had been working before SWIFT 4.1 version
+        let testUUID:String? = UIDevice.current.identifierForVendor?.uuidString
+        
+        if let UUID = testUUID  {
+            indentifier.text = UUID
+        } else {
+            indentifier.text = "It doesn't work"
+        }
+        
+        
     }
 
+    
 
 }
 
